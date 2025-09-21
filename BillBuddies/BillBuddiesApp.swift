d1688bd0ccb57e687f1046c68b42d303f79bdedc
Ritesh <riteshkhore@gmail.com>
2025-09-21 20:08:20 +0530

    Initial Commit

---
+++ BillBuddies/BillBuddiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillBuddiesApp.swift
+//  BillBuddies
+//
+//  Created by Ritesh Khore on 21/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BillBuddiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
