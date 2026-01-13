1c2a2c59957bf843d2737ee645bd64fbe296ca42
Ritesh Khore <82861420+ritesh-15@users.noreply.github.com>
2026-01-13 21:39:37 +0530

    feat: Implements group details endpoint (#17)
    - Added group details endpoint and fetched group details based on ID

--- BillBuddies/Screens/Main/Presentation/Tabs/Groups/Data/Repository/GroupsRepository.swift
+++ BillBuddies/Screens/Main/Presentation/Tabs/Groups/Data/Repository/GroupsRepository.swift
@@ -5,6 +5,8 @@ protocol GroupsRepositoryProtocol: AnyObject {
     func fetchGroups(userId: String) async -> Result<[GroupsModel], NetworkError>
 
     func createGroup(group: CreateGroupModel) async -> Result<CreateGroupResponseModel, NetworkError>
+
+    func fetchGroupDetails(groupId: String) async -> Result<GroupDetailResponseModel, NetworkError>
 }
 
 final class GroupsRepository: GroupsRepositoryProtocol {
@@ -37,6 +39,39 @@ final class GroupsRepository: GroupsRepositoryProtocol {
             switch result {
             case .success(let data):
                 return .success(GroupsResponseMapper.toDomain(data))
+            case .failure(let networkError):
+                return .failure(networkError)
+            }
+        } catch let error as NetworkError {
+            return .failure(error)
+        } catch _ {
+            return .failure(.unknown)
+        }
+    }
+
+    func fetchGroupDetails(groupId: String) async -> Result<GroupDetailResponseModel, NetworkError> {
+        do {
+            let request = try NetworkRequestBuilder()
+                .method(method: .get)
+                .setPath(path: "/groups/\(groupId)")
+                .addHeader(key: "Content-Type", value: "application/json")
+                .addQuery("populate[creator][fields][0]", value: "id")
+                .addQuery("populate[creator][fields][1]", value: "username")
+                .addQuery("populate[members][fields][0]", value: "id")
+                .addQuery("populate[members][fields][1]", value: "username")
+                .addQuery("populate[expenses][fields][0]", value: "id")
+                .addQuery("populate[expenses][fields][1]", value: "description")
+                .addQuery("populate[expenses][fields][2]", value: "amount")
+                .addQuery("populate[expenses][populate][paidBy][fields][0]", value: "id")
+                .addQuery("populate[expenses][populate][splitShares][fields][0]", value: "id")
+                .addQuery("populate[expenses][populate][splitShares][populate][ownedBy][fields][0]", value: "id")
+                .build()
+
+            let result: NetworkResult<DTOGroupDetailResponse> = await networkService.request(request)
+
+            switch result {
+            case .success(let data):
+                return .success(GroupDetailsResponseMapper.toDomain(data))
             case .failure(let networkError):
                 return .failure(networkError)
             }
